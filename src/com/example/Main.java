109db37c52408f122be666dafaf82ced04024170
Qsior <lukaszbojes@gmail.com>
2017-12-28 23:50:20 +0100

    Added method to sort an array

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -23,28 +23,61 @@ public class Main {
 //
 //	      double[] myDoubleArray = new double[10];
 //        System.out.println(myIntArray[0];
-        int[] myIntegers = getIntegers(5);
-        for(int i = 0 ; i < myIntegers.length; i++){
-            System.out.println("Element " + i + ", value = " + myIntegers[i]);
-        }
+//        int[] myIntegers = getIntegers(5);
+//        for(int i = 0 ; i < myIntegers.length; i++){
+//            System.out.println("Element " + i + ", value = " + myIntegers[i]);
+//        }
+//
+//        System.out.println("The average is " + getAverage(myIntegers));
+//    }
+//
+//    public static int[] getIntegers(int number){
+//        System.out.println("Enter " + number + " integer values.\r");
+//        int[] values = new int[number];
+//        for (int i = 0 ; i < values.length; i++){
+//            values[i] = scanner.nextInt();
+//        }
+//        return values;
+//    }
+//
+//    public static double getAverage(int[] array){
+//        int sum = 0;
+//        for(int i = 0 ; i < array.length ; i++){
+//            sum += array[i];
+//        }
+//        return (double) sum / array.length;
 
-        System.out.println("The average is " + getAverage(myIntegers));
+        printArray(sortIntegers(getIntegers()));
+
     }
 
-    public static int[] getIntegers(int number){
-        System.out.println("Enter " + number + " integer values.\r");
-        int[] values = new int[number];
-        for (int i = 0 ; i < values.length; i++){
-            values[i] = scanner.nextInt();
+    public static int[] getIntegers(){
+        int[] array = new int[5];
+        System.out.println("Enter " + array.length + " integer values\r");
+        for(int i = 0 ; i < array.length; i++){
+            array[i] = scanner.nextInt();
         }
-        return values;
+        return array;
     }
 
-    public static double getAverage(int[] array){
-        int sum = 0;
+    public static void printArray(int[] array){
+        for(int i = 0; i < array.length; i++){
+            System.out.println("Element " + (i+1) + " : " + array[i]);
+        }
+    }
+
+    public static int[] sortIntegers(int[] array){
+        int oldInt;
         for(int i = 0 ; i < array.length ; i++){
-            sum += array[i];
+            for(int j = i+1 ; j < array.length; j++){
+                if(array[i] < array[j]) {
+                    oldInt = array[i];
+                    array[i] = array[j];
+                    array[j] = oldInt;
+                }
+            }
         }
-        return (double) sum / array.length;
+        return array;
     }
+
 }
